d5d4cefa200c5e78669b52d8c9a9481eb33d3149
alberto <alberto@albertopc>
2018-07-23 19:50:07 +0200

    first commit

--- model.js
+++ model.js
@@ -1,19 +1,25 @@
+var user;
 window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 var db;
 var requestDB = window.indexedDB.open("paniniDB", 1);
+var StorageHelper = {
+  initUser : function(user) {
+    var request = db.transaction("global").objectStore("global").get("user");
+    request.onsuccess = function(event) {
+      user =  event.target.result.name;
+    };
+    request.onerror = function(event) {
+      alert("ObjectStore:"+event);
+    }
+  }
+}
 requestDB.onerror = function(event) {
   alert("Attenzione, il database locale di paniniweb non può essere aperto.\nDatabase error: " + event.target.errorCode);
 };
 requestDB.onsuccess = function(event) {
   db = event.target.result;
+  StorageHelper.initUser(user);
 };
-var StorageHelper = {
-  initUser : function(user) {
-    db.transaction("user").objectStore("user").get("user").onsuccess = function(event) {
-      user =  event.target.result.name;
-    };
-  }
-}
 function Volontario() {
   this.email="";
   this.nome= "";
@@ -28,5 +34,3 @@ Volontario.prototype.setPassword = function(value) {
 function evento() {
   ;
 }
-var user;
-StorageHelper.initUser(user);
