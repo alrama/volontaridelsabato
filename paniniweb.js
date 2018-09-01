d018c4a687efb584d8a527fb246dab4e08a4b57c
alberto <alberto@albertopc>
2018-09-01 17:19:02 +0200

    first commit

--- paniniweb.js
+++ paniniweb.js
@@ -12,18 +12,34 @@ function showMessage(message) {
   window.scrollTo(0,0);
 }
 function displayVolontari() {
-
+  showMessage("");
+  var htmlcontant = "";
+  for (volontario of volontari) {
+    htmlcontant += "<div class='riga'><div class='textsmall dettaglio_riga'>";
+    if (volontario.nome.length+volontario.cognome.length<5) {
+      htmlcontant+=volontario.nome + " " + volontario.cognome + "</div>";
+      htmlcontant+="<div class='textsmall dettaglio_riga'>"+volontario.email+"</div>";
+    } else {
+      htmlcontant+=volontario.nome + "</div>";
+      htmlcontant+="<div class='textsmall dettaglio_riga'>"+volontario.cognome+"</div>";
+    }
+    htmlcontant+="</div>"
+  }
+  $("#volontari_content").html(htmlcontant);
+}
+function aggiornaVolontari() {
+  $("#message").text('Attendere. Richiesta dati remoti');
+  NetworkHelper.loadVolontari(function() {
+    if (volontari) {
+      displayVolontari();
+    } else showMessage('Errore. Impossibile ricevere la lista di volontari');
+  });
 }
 function loadVolontari() {
   showMessage('Attendere. Caricamento dati in corso');
   StorageHelper.getVolontari(function() {
     if (volontari==undefined || volontari.length==0) {
-      $("#message").text('Attendere. Richiesta dati remoti');
-      NetworkHelper.loadVolontari(function() {
-        if (volontari) {
-          displayVolontari();
-        } else showMessage('Errore. Impossibile ricevere la lista di volontari');
-      });
+      aggiornaVolontari();
     } else {
       showMessage("");
       displayVolontari();
