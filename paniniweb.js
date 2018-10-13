8df810d18744ca7aaa350a6e01f9eb6c51080c07
alberto <alberto@albertopc>
2018-10-13 17:13:19 +0200

    first release

--- paniniweb.js
+++ paniniweb.js
@@ -30,18 +30,18 @@ function displayVolontari() {
   showMessage("");
   var htmlcontant = "";
   for (volontario of volontari) {
-    htmlcontant += "<div class='riga' onclick='dettaglio_riga(this,\""+volontario.email+"\")'><div><div class='textsmall'>";
+    htmlcontant += "<div class='riga' onclick='dettaglio_riga(this,\""+volontario.email+"\")'><div class='textsmall'>";
     if (volontario.nome.length+volontario.cognome.length<5) {
       htmlcontant+=volontario.nome + " " + volontario.cognome + "</div>";
       htmlcontant+="<div class='textsmall'>"+volontario.email+"</div>";
     } else {
       htmlcontant+=volontario.nome + "</div>";
-      htmlcontant+="<div class='textsmall dettaglio_riga'>"+volontario.cognome+"</div></div>";
+      htmlcontant+="<div class='textsmall dettaglio_riga'>"+volontario.cognome+"</div>";
     }
-    if (navigator.userAgent.toLowerCase().indexOf('mobile')>-1) {
-      htmlcontant+="<a href='tel:+39"+volontario.cellulare+"'><img src='img/phone-forward.png' class='img_title icon_header'/></a>";
-      htmlcontant+="<a href='https://api.whatsapp.com/send?phone=39"+volontario.cellulare+"'><img src='img/chat-processing.png' class='img_title icon_header'/></a>"
-    } else htmlcontant+="<img  src='img/phone-forward.png' style='visibility:hidden;height:40px;vertical-align:middle'/>";
+    if (navigator.userAgent.toLowerCase().indexOf('mobile')>-1 && volontario.cellulare!=null && volontario.cellulare!="") {
+      htmlcontant+="<a href='tel:+39"+volontario.cellulare+"'><img src='img/phone-forward.png' class='img_title icon_header' style='float:right;margin-top:-40px;margin-right:60px'/></a>";
+      htmlcontant+="<a href='https://api.whatsapp.com/send?phone=39"+volontario.cellulare+"'><img src='img/chat-processing.png' class='img_title icon_header' style='float:right;margin-top:-40px'/></a>"
+    }
     htmlcontant+="</div>"
   }
   $("#volontari_content").html(htmlcontant);
